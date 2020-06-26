d6211724b9d1d18540e1c63b298907e0210edf9b
ClydeFrog04 <egan.randal@gmail.com>
2020-06-25 19:31:30 -0600

    stretch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Route} from 'react-router-dom';
 import data from './data';
 
@@ -8,20 +8,38 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import {ProductContext} from "./contexts/ProductContext";
 import {CartContext} from "./contexts/CartContext";
+import Checkout from "./components/Checkout";
 
 function App() {
     const [products] = useState(data);
     const [cart, setCart] = useState([]);
 
+    //shopping cart persisting on refresh now :] <3
+    useEffect(() =>{
+        //set initial cart base on localStoreage here
+        setCart(JSON.parse(localStorage.getItem("shoppingCart")));
+    },[]);
+
+    useEffect(() =>{
+        //set local storeage here
+        localStorage.setItem("shoppingCart", JSON.stringify(cart));
+    },[cart]);
+
     const addItem = item => {
         // add the given item to the cart
         setCart([...cart, item]);
     };
+    const removeItem = itemId =>{
+        const removedItemCart = cart.filter(item =>{
+            if(item.id !== itemId) return item;
+        });
+        setCart(removedItemCart);
+    }
 
     return (
         <div className="App">
             <ProductContext.Provider value={{products, addItem}}>
-                <CartContext.Provider value={{cart}}>
+                <CartContext.Provider value={{cart, removeItem}}>
                     <Navigation/>
 
                     {/* Routes */}
@@ -31,6 +49,9 @@ function App() {
 
                     <Route path="/cart">
                         <ShoppingCart/>
+                    </Route>
+                    <Route path="/checkout">
+                        <Checkout/>
                     </Route>
                 </CartContext.Provider>
             </ProductContext.Provider>
---
+++ src/components/Checkout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Checkout = () =>{
+
+    return (
+        <div className="checkout">
+
+        </div>
+    );
+}
+
+export default Checkout;
